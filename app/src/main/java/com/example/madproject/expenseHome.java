4c4ac401763b06c3a40584420fbc8c81f4d07a00
[Sangeeth] <sjack7775@gmail.com>
2019-09-21 23:19:36 +0530

    change expense home page

--- app/src/main/java/com/example/madproject/expenseHome.java
+++ app/src/main/java/com/example/madproject/expenseHome.java
@@ -3,11 +3,11 @@ package com.example.madproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class expense extends AppCompatActivity {
+public class expenseHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_expense);
+        setContentView(R.layout.activity_expense_home);
     }
 }
