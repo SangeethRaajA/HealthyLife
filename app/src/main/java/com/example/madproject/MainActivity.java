cfeeaa9c56840f8b4bc5d0b37b2aebb05d91ba5e
[Sangeeth] <sjack7775@gmail.com>
2019-09-21 12:59:48 +0530

    added intent to bmi and expense

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -24,58 +24,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Button mainButton = findViewById(R.id.mainButton);
-
-
     }
-
-    public void viewMyMain(View v)
-    {
-        Intent i = new Intent(this,Add_ToDo.class);
-        startActivity(i);
-
-    }
-
-
-    /*public void record(View v){
 
     public void reminder(View v){
-
-
         Intent intent = new Intent(this,Add_ToDo.class);
-        //Intent intent = new Intent(this,expense_home.class);
         startActivity(intent);
     }
-
-    //TEST 02
-    //GUYS REMAIN USE THIS ACTIVITY AND CREATE YOUR INTENDS
 
     public void bmi(View v){
-
-
-    //test 03 add branch
-    /*public void bmi(View v){
-
-
-        Intent intent = new Intent(MainActivity.this,BMI.class);
-        //Intent intent = new Intent(this,expense_home.class);
-        startActivity(intent);
-    }*/
-
-    public void diet(View v){
-
-        Intent intent = new Intent(this,diet.class);
-        //Intent intent = new Intent(this,expense_home.class);
+        Intent intent = new Intent(MainActivity.this,bmi.class);
         startActivity(intent);
     }
 
-   /* public void reminder(View v){
-
-        Intent intent = new Intent(MainActivity.this,REMINDER.class);
-        //Intent intent = new Intent(this,expense_home.class);
+    public void diet(View v){
+        Intent intent = new Intent(this,diet.class);
         startActivity(intent);
-    }*/
+    }
 
-//commit
+    public void expense(View v){
+        Intent intent = new Intent(MainActivity.this,expense.class);
+        startActivity(intent);
+    }
+
+
 }
